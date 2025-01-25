01a441871861a7d2bb30c8a1cc51753636339350
Vernon <vernonwilks@gmail.com>
2025-01-25 15:52:00 +0200

    updated the file

---
+++ go-project/investment_calculator.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	const inflationRate float64 = 2.5
+	var investmentAmount float64
+	var years float64
+	var expectedReturnRate float64
+
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	fmt.Print("Expected Rate of Return: ")
+	fmt.Scan(&expectedReturnRate)
+
+	futureValue := (investmentAmount) * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
+}
